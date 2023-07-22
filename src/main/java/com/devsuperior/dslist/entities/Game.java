34ba157ff87720019f7b56de1db3aa0f7c4864d9
Rodrigo Cerqueira <rodrigocerqueira@outlook.com>
2023-07-21 22:30:01 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -17,24 +17,28 @@ public class Game {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
-	
+
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
 	private String shortDescription;
-	private String longDescription;
 	
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+
 	private Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, Double score, String imgUrl, String shortDescription,
-			String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -71,6 +75,14 @@ public class Game {
 
 	public void setGenre(String genre) {
 		this.genre = genre;
+	}
+
+	public String getPlatforms() {
+		return platforms;
+	}
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public Double getScore() {

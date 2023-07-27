5bcff0950e1fd85961b24db4dba60cc5e4403bd4
Rodrigo Cerqueira <rodrigocerqueira@outlook.com>
2023-07-26 22:54:38 -0300

    GET game by lists

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,11 +1,21 @@
 package com.devsuperior.dslist.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
+    @Query("SELECT new com.devsuperior.dslist.dto.GameMinDTO(g.id, g.title, g.year, g.imgUrl, g.shortDescription) " +
+            "FROM Game g " +
+            "JOIN Belonging b ON g.id = b.id.game.id " +
+            "WHERE b.id.list.id = :listId " +
+            "ORDER BY b.position")
+     List<GameMinProjection> searchByList(Long listId);
 
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import com.devsuperior.dslist.services.exceptions.ResourceNotFoundException;
 
@@ -29,6 +30,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(GameMinDTO::new).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(GameMinDTO::new).toList();
 	}
 }
